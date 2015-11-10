abb7221757286ec311e2e0a8ebdf41337c58f38f
SJTreadway <treadway08@gmail.com>
2015-11-10 12:19:24 -0600

    updates time every second

--- js/timeDirective.js
+++ js/timeDirective.js
@@ -1,12 +1,14 @@
 (function() {
   angular.module('timeApp')
-         .directive('showTime', function() {
+         .directive('showTime', function($interval) {
            return {
              restrict: 'E',
              template: '<div> The current time is {{time}} </div>',
              link: function(scope, element, attrs) {
-                  var currentTime = new Date();
-                  scope.time = currentTime.toString();
+                  scope.time = new Date().toString();
+                  $interval(function() {
+                    scope.time = new Date().toString();
+                  }, 1000)
              }
            }
          })
